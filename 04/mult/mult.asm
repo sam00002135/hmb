d040ff37a3b18bfa25c5a17e536763c9e3fd946f
sam00002135 <104470583+sam00002135@users.noreply.github.com>
2022-11-24 11:24:26 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+@0
+D=M
+@END
+D;JEQ
+@1
+D=M
+@END
+D;JEQ
+@0
+D=M
+@3
+M=D
+(LOOP)
+@1
+D=M
+@2
+M=D+M
+@3
+M=M-1
+D=M
+@LOOP
+D;JGT
+(END)
+@END
+0;JMP
